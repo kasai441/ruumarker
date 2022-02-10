f8681591bfb84d061e329e19bb244047c5975ed3
kasai441 <kasai441441@gmail.com>
2022-02-10 21:36:48 +0900

    マップ更新機能のテストを追加した #37

--- spec/system/maps_spec.rb
+++ spec/system/maps_spec.rb
@@ -7,13 +7,43 @@ describe 'マップ管理機能', type: :system do
     before do
       visit new_room_path
       fill_in 'Trimming', with: '10, 20'
-      fill_in 'Expansion', with: '50'
-      fill_in 'Rotation', with: '45'
+      fill_in 'Expansion', with: '30'
+      fill_in 'Rotation', with: '40'
       click_button '登録する'
     end
 
     it '正常に登録される' do
       expect(page).to have_selector '.alert-success', text: '登録しました'
+      expect(page).to have_selector '.trimming', text: '10, 20'
+      expect(page).to have_selector '.expansion', text: '30'
+      expect(page).to have_selector '.rotation', text: '40'
+    end
+  end
+
+  describe '更新機能' do
+    let(:room1) { FactoryBot.create(:room) }
+    let!(:map1) do
+      FactoryBot.create(:map,
+                        trimming: '100, 200',
+                        expansion: '300',
+                        rotation: '400',
+                        room: room1)
+    end
+
+    before do
+      visit room_path(room1)
+      click_link 'マップ編集'
+      fill_in 'Trimming', with: '1, 2'
+      fill_in 'Expansion', with: '3'
+      fill_in 'Rotation', with: '4'
+      click_button '更新する'
+    end
+
+    it '正常に登録される' do
+      expect(page).to have_selector '.alert-success', text: '変更しました'
+      expect(page).to have_selector '.trimming', text: '1, 2'
+      expect(page).to have_selector '.expansion', text: '3'
+      expect(page).to have_selector '.rotation', text: '4'
     end
   end
 end
