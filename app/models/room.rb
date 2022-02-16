0171d48fbe079a8487009d28a451f0dd0988c199
kasai441 <kasai441441@gmail.com>
2022-02-16 11:55:35 +0900

    ルームcreateのコールバックでIDがランダム20文字になるようにした #57

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,13 @@
 
 class Room < ApplicationRecord
   has_one :map, dependent: :destroy
+  before_create :set_id
+
+  private
+
+  def set_id
+    while id.blank? || Room.find_by(id: id).present? # rubocop:disable Style/WhileUntilModifier, Style/HashSyntax
+      self.id = SecureRandom.alphanumeric(20)
+    end
+  end
 end
