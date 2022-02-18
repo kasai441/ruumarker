a90eec2e24b4d5d622cd8616fdd30121ba64566a
kasai441 <kasai441441@gmail.com>
2022-02-18 17:36:56 +0900

    ルームコントローラーからnewを外す

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'marks/new'
-  get 'marks/create'
-  get 'marks/edit'
-  get 'marks/update'
-  get 'marks/destroy'
   # あとでやる：ルートをランダムURLにする方法
-  root to: 'rooms#new'
+  root to: 'rooms#create'
   resources :rooms, except: %i[index edit create] do
     resources :maps, except: %i[index show destroy]
     resources :marks, except: :index
