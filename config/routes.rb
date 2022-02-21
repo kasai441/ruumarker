31910bac3c959eab80a75a039fc3d6f8c2bfdf88
kasai441 <kasai441441@gmail.com>
2022-02-21 12:05:12 +0900

    更新機能テストの更新とテストコードの記述統一

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # あとでやる：ルートをランダムURLにする方法
   root to: 'home#index'
   resources :rooms, except: %i[index edit create] do
     resources :maps, except: %i[index show destroy]
