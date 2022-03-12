6b2256cf4ef0db8e30f966b8925b88321cd3826d
kasai441 <kasai441441@gmail.com>
2022-03-12 10:56:36 +0900

    debug用に書き込んだコードを修正 #95

--- app/javascript/modules/posts.js
+++ app/javascript/modules/posts.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const apiUrlBase = '/api/marks'
-const headers = { 'Content-Type': 'multipart/form-data'
-}
+const headers = { 'Content-Type': 'multipart/form-data' }
 axios.defaults.headers.common = {
   'X-Requested-With': 'XMLHttpRequest',
   'X-CSRF-TOKEN' : document.querySelector('meta[name="csrf-token"]').getAttribute('content')
@@ -12,27 +11,13 @@ const state = {
 }
 
 const getters = {
-  posts: state => {
-    console.log('getters:')
-    console.log(state)
-    return state.posts.sort((a, b) => b.id - a.id)
-  }
+  posts: state => state.posts.sort((a, b) => b.id - a.id)
 }
 
 const mutations = {
-  setPosts: (state, posts) => {
-    console.log('setPosts:')
-    console.log(state)
-    console.log(posts)
-    return (state.posts = posts)
-  },
-  appendPost: (state, post) => {
-    console.log('appendPosts:')
-    console.log(state)
-    return (state.posts = [...state.posts, post])
-  },
-  removePost: (state, id) =>
-    (state.posts = state.posts.filter(post => post.id !== id))
+  setPosts: (state, posts) => (state.posts = posts),
+  appendPost: (state, post) => (state.posts = [...state.posts, post]),
+  removePost: (state, id) => (state.posts = state.posts.filter(post => post.id !== id))
 }
 
 const actions = {
