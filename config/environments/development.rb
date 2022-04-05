30b40a239b959952b2a227c96cb70a3bd1889ce1
kasai441 <kasai441441@gmail.com>
2022-04-05 15:11:23 +0900

    ローカルのproductionとdevelopmentの切り替えのために必要だと思って追加したが不要だったassets.debugの設定を削除した #72

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,8 +69,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  # https://interfirm.hatenablog.com/entry/2014/10/24/131503
-  # ローカルで本番環境のプリコンパイルを一度すると開発環境でもプリコンパイルが必要になってしまうのを、直す
-  config.assets.debug = false
 end
