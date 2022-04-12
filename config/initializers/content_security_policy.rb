ee4da9e44817f8a8ee548d2ca8353bd8a057ef31
kasai441 <kasai441441@gmail.com>
2022-04-12 13:54:34 +0900

    スクリプト以外のアセットのCSPを設定した #72

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -7,12 +7,12 @@
 
 Rails.application.configure do
   config.content_security_policy do |policy|
-    # policy.default_src :self, :https
-    # policy.font_src    :self, :https, :data
-    # policy.img_src     :self, :https, :data
-    # policy.object_src  :none
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
     policy.script_src  :self, :unsafe_eval, :https
-    # policy.style_src   :self, :https
+    policy.style_src   :self, :https
     # Specify URI for violation reports
     # policy.report_uri "/csp-violation-report-endpoint"
   end
