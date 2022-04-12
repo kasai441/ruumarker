47e137863c76b107a33dc8b8b6cf978e7dc4f6c1
kasai441 <kasai441441@gmail.com>
2022-04-12 14:20:48 +0900

    CSPのnonce設定を有効化した #72

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -18,8 +18,8 @@ Rails.application.configure do
   end
 
   # # Generate session nonces for permitted importmap and inline scripts
-  # config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
-  # config.content_security_policy_nonce_directives = %w(script-src)
+  config.content_security_policy_nonce_generator = ->(request) { request.session.id.to_s }
+  config.content_security_policy_nonce_directives = %w(script-src)
 
   # Report CSP violations to a specified URI. See:
   # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
