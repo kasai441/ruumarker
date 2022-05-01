22dc660e14addc8e66a7177dfd84e7012c51b4bd
kasai441 <kasai441441@gmail.com>
2022-05-01 14:50:27 +0900

    vueで反映されるようにconfigにvue拡張子を追加した #116

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
     './app/views/**/*.html.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
+    './app/javascript/**/*.js',
+    './app/javascript/**/*.vue'
   ],
   plugins: [
     require('daisyui')
