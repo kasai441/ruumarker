83bb460a3e069dc65cd65f6a42e640fd83b9b74a
kasai441 <kasai441441@gmail.com>
2022-05-06 15:41:20 +0900

    画像詳細画面での表示をpixelから割合にかえたtrimmingに対応した #145

--- app/javascript/components/image_show.vue
+++ app/javascript/components/image_show.vue
@@ -35,7 +35,9 @@ export default {
   data() {
     return {
       formData: new FormData(),
-      imageUrl: null
+      imageUrl: null,
+      showFieldWidth: 0,
+      showFieldHeight: 0
     }
   },
   methods: {
@@ -51,23 +53,38 @@ export default {
 
       if (imageFile) this.formData.set(`${this.targetModel}[image]`, imageFile)
       this.$emit('emitFormData', this.formData)
+    },
+    getFieldSize() {
+      const showField = document.getElementById('show-field')
+      const showFieldLeft = Math.floor(showField.getBoundingClientRect().left)
+      const showFieldTop = Math.floor(showField.getBoundingClientRect().top)
+      this.showFieldWidth = Math.floor(showField.getBoundingClientRect().right) - showFieldLeft
+      this.showFieldHeight = Math.floor(showField.getBoundingClientRect().bottom) - showFieldTop
+
+      const showImage = document.getElementById('show-image')
+      const trimming = params.trimming(this.formData, this.targetModel)
+      showImage.style.left = Math.floor(this.showFieldWidth * trimming.x) + 'px'
+      showImage.style.top = Math.floor(this.showFieldHeight * trimming.y) + 'px'
+    },
+    handleResize() {
+      this.getFieldSize()
     }
   },
   async created() {
     const response = await api.actions.show(`/api/rooms/${this.roomId}/${this.targetModel}s/${this.id}.json`)
-
     this.formData.append(`${this.targetModel}[trimming]`, response.trimming)
     this.formData.append(`${this.targetModel}[image_url]`, response.image_url)
     this.$emit('emitFormData', this.formData)
-
     this.imageUrl = response.image_url
   },
+  mounted() {
+    window.addEventListener('resize', this.handleResize)
+  },
   updated() {
-    const showImage = document.getElementById('show-image')
-    const trimming = params.trimming(this.formData, this.targetModel)
-    console.log(trimming)
-    showImage.style.left = trimming.x + 'px'
-    showImage.style.top = trimming.y + 'px'
+    this.getFieldSize()
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.handleResize)
   }
 }
 </script>
