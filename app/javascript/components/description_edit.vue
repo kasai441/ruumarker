2b6507f0e08bcfb19e811666c7989a813f4b1855
kasai441 <kasai441441@gmail.com>
2022-05-17 14:59:14 +0900

    キズ概要のフォームデータも更新毎にリニューアルするように。updatedでやると無限ループになるので、フォームからフォーカスアウトしたタイミングでやる。 #125

--- app/javascript/components/description_edit.vue
+++ app/javascript/components/description_edit.vue
@@ -4,11 +4,13 @@
       <label class="label">
         <span class="label-text">キズの概要を記入してください</span>
       </label>
-      <input type="text" placeholder="キズ概要" v-model="description" id="edit-description" class="input input-bordered w-full" />
+      <input type="text" placeholder="キズ概要" v-model="description" @blur="emitData" id="edit-description" class="input input-bordered w-full" />
     </div>
   </section>
 </template>
 <script>
+import params from '../modules/params'
+
 export default {
   name: 'DescriptionEdit',
   props: [
@@ -20,12 +22,15 @@ export default {
       description: null
     }
   },
+  methods: {
+    emitData() {
+      const formData = params.renewFormData(this.formData)
+      formData.set(`${this.targetModel}[description]`, this.description)
+      this.$emit('emitFormData', formData)
+    }
+  },
   mounted() {
     this.description = this.formData.get(`${this.targetModel}[description]`)
-  },
-  updated() {
-    this.formData.set(`${this.targetModel}[description]`, this.description)
-    this.$emit('emitFormData', this.formData)
   }
 }
 </script>
