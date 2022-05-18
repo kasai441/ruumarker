e3d637056d91326c72e0f796383270363f687b23
kasai441 <kasai441441@gmail.com>
2022-05-18 09:26:00 +0900

    画像位置編集コンポーネントを追加した #125

---
+++ app/javascript/components/location_edit.vue
@@ -0,0 +1,121 @@
+<template>
+  <section id="location-edit">
+    <div id="location-field" @pointermove="touchmove($event)" @pointerup="touchend($event)" @pointerleave="touchend($event)"
+         class="my-16 edit-size">
+      <img :src="imageUrl" id="location-image" draggable="false" @pointerdown="touchstart($event)"
+           class="absolute z-10 edit-size object-contain">
+      <div class="relative">
+        <div class="absolute z-30 edit-size pointer-events-none bg-transparent outline outline-4 outline-lime-500"></div>
+        <div class="absolute z-20 edit-size pointer-events-none bg-transparent outline outline-112 outline-slate-200 opacity-40"></div>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import params from '../modules/params'
+
+export default {
+  name: 'LocationEdit',
+  props: [
+    'formData',
+    'mapFormData',
+    'targetModel'
+  ],
+  data() {
+    return {
+      trimming: null,
+      imageUrl: null,
+      isMovable: false,
+      editFieldLeft: 0,
+      editFieldTop: 0,
+      editFieldWidth: 0,
+      editFieldHeight: 0,
+      editImage: null,
+      editImageLeft: 0,
+      editImageTop: 0,
+      shiftX: 0,
+      shiftY: 0
+    }
+  },
+  methods: {
+    touchstart(e) {
+      this.isMovable = true
+      this.editImage.classList.add('shadow-2xl')
+      this.shiftX = Math.floor(e.clientX) - this.editImageLeft
+      this.shiftY = Math.floor(e.clientY) - this.editImageTop
+    },
+    touchmove(e) {
+      if (!this.isMovable) return
+      this.editImageLeft = Math.floor(e.pageX) - this.shiftX
+      this.editImageTop = Math.floor(e.pageY) - this.shiftY
+
+      // 外側に出ないように画像の移動を抑制する
+      const constrainRangeX = Math.floor(this.editFieldWidth / 4)
+      const constrainRangeY = Math.floor(this.editFieldHeight / 4)
+      const maxLeft = this.editFieldLeft + constrainRangeX
+      const maxTop = this.editFieldTop + constrainRangeY
+      const minLeft = maxLeft - constrainRangeX * 2
+      const minTop = maxTop - constrainRangeY * 2
+      if (this.editImageLeft > maxLeft) this.editImageLeft = maxLeft
+      if (this.editImageLeft < minLeft) this.editImageLeft = minLeft
+      if (this.editImageTop > maxTop) this.editImageTop = maxTop
+      if (this.editImageTop < minTop) this.editImageTop = minTop
+
+      this.editImage.style.left = this.editImageLeft + 'px'
+      this.editImage.style.top = this.editImageTop + 'px'
+    },
+    touchend() {
+      this.isMovable = false
+      this.editImage.classList.remove('shadow-2xl')
+      this.updateTrimming()
+    },
+    handleResize() {
+      this.getFieldSize()
+      this.updateTrimming()
+    },
+    getFieldSize() {
+      const editField = document.getElementById('location-field')
+      if(editField) {
+        this.editFieldLeft = Math.floor(editField.getBoundingClientRect().left)
+        this.editFieldTop = Math.floor(editField.getBoundingClientRect().top)
+        this.editFieldWidth = Math.floor(editField.getBoundingClientRect().right) - this.editFieldLeft
+        this.editFieldHeight = Math.floor(editField.getBoundingClientRect().bottom) - this.editFieldTop
+
+        console.log(`ImageEdit.editFieldTop: ${this.editFieldTop}`)
+        this.editImage = document.getElementById('location-image')
+        this.editImageLeft = Math.floor(this.editFieldWidth * this.trimming.x) + this.editFieldLeft
+        this.editImageTop = Math.floor(this.editFieldHeight * this.trimming.y) + this.editFieldTop
+        this.editImage.style.left = this.editImageLeft + 'px'
+        this.editImage.style.top = this.editImageTop + 'px'
+      }
+    },
+    updateTrimming() {
+      const trimmingX = ((this.editImageLeft - this.editFieldLeft) / this.editFieldWidth).toFixed(3)
+      const trimmingY = ((this.editImageTop - this.editFieldTop) / this.editFieldHeight).toFixed(3)
+      const formData = params.renewFormData(this.formData)
+      formData.set(`${this.targetModel}[trimming]`, JSON.stringify({x: trimmingX, y: trimmingY}))
+      // this.$emit('emitFormData', formData)
+    }
+  },
+  mounted() {
+    console.log('ImageEdit#mounted')
+
+    window.addEventListener('resize', this.handleResize)
+
+    this.imageUrl = this.formData.get(`${this.targetModel}[image_url]`)
+    this.trimming = params.trimming(this.formData, this.targetModel)
+    this.getFieldSize()
+  },
+  async updated() {
+    console.log('LocationEdit#updated')
+    // const imageFile = this.formData.get(`${this.targetModel}[image]`)
+    // if (!imageFile) return
+    //
+    // const editImage = document.getElementById( 'location-image' )
+    // editImage.src = await params.getImageUrl(imageFile)
+  },
+  beforeDestroy: () => {
+    window.removeEventListener('resize', this.handleResize)
+  }
+}
+</script>
--- app/javascript/views/marks_edit.vue
+++ app/javascript/views/marks_edit.vue
@@ -7,6 +7,8 @@
         <image-edit :form-data="formData" target-model="mark" @emit-form-data="getFormData"></image-edit>
         <image-upload :form-data="formData" target-model="mark" @emit-form-data="getFormData"></image-upload>
         <description-edit :form-data="formData" target-model="mark" @emit-form-data="getFormData"></description-edit>
+        <h2 class="w-full p-1 text-sm relative z-50 relative z-50">キズの位置を編集してください</h2>
+        <location-edit :form-data="formData" :map-form-data="mapFormData" target-model="mark" @emit-form-data="getFormData"></location-edit>
         <image-update :room-id="roomId" :form-data="formData" target-model="mark"></image-update>
       </div>
     </div>
@@ -17,6 +19,7 @@
 import ImageEdit from '../components/image_edit.vue'
 import ImageUpload from '../components/image_upload.vue'
 import DescriptionEdit from '../components/description_edit.vue'
+import LocationEdit from '../components/location_edit.vue'
 import ImageUpdate from '../components/image_update.vue'
 import params from '../modules/params'
 
@@ -25,17 +28,19 @@ export default {
   inject: [
     'roomId',
     'mark',
-    // 'map'
+    'map'
   ],
   data() {
     return {
-      formData: null
+      formData: null,
+      mapFormData: null
     }
   },
   components: {
     ImageEdit,
     ImageUpload,
     DescriptionEdit,
+    LocationEdit,
     ImageUpdate
   },
   methods: {
@@ -51,6 +56,8 @@ export default {
     this.formData = params.initFormData(this.mark, 'mark')
     const imageUrl = this.formData.get('mark[image_url]')
     if (!imageUrl) this.formData.append('mark[image_url]', '/sample.png')
+
+    this.mapFormData = params.initFormData((this.map, 'map'))
   }
 }
 </script>
