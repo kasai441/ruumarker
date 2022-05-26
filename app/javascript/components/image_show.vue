3927ad54c820d4bc81647c8507f30c4dd642175e
kasai441 <kasai441441@gmail.com>
2022-05-26 14:04:20 +0900

    ボタンだった場合にリスナーをつけない処理を追加した #175

--- app/javascript/components/image_show.vue
+++ app/javascript/components/image_show.vue
@@ -92,6 +92,8 @@ export default {
       }
     },
     visitLocators(e) {
+      if (e.path[0].childNodes[0].classList.value.includes('btn')) return
+
       location.href = `/rooms/${this.roomId}/${this.locatorsModel}s/${e.path[1].id.replace('mark-', '')}/edit`
     },
     handleResize() {
