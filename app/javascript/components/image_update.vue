91c5528135ddaa2382d1409ad516dae18c3fee40
kasai441 <kasai441441@gmail.com>
2022-05-29 12:30:39 +0900

    ボタンが真ん中になっていなかったのでマージンを調整した #26

--- app/javascript/components/image_update.vue
+++ app/javascript/components/image_update.vue
@@ -1,9 +1,11 @@
 <template>
   <section id="image-update">
-    <div class="w-field flex justify-center items-center p-4">
-      <a @click="back" id="show" class="w-1/3 text-xl text-slate-400 btn btn btn-ghost">＜</a>
-      <a @click="update" id="update" class="w-1/3 btn btn-lime">変更</a>
-      <div class="w-1/3"></div>
+    <div class="flex justify-center p-4">
+      <div class="w-field flex items-center">
+        <a @click="back" id="show" class="w-1/3 text-xl text-slate-400 btn btn btn-ghost">＜</a>
+        <a @click="update" id="update" class="w-1/3 btn btn-lime">変更</a>
+        <div class="w-1/3"></div>
+      </div>
     </div>
   </section>
 </template>
