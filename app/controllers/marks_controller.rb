4646bab383df2a41214ea7c360760607efd4863c
kasai441 <kasai441441@gmail.com>
2022-06-09 11:05:22 +0900

    brief_descriptionを消去 #202

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -10,10 +10,4 @@ class MarksController < ApplicationController
   def edit
     @mark = Mark.find(params[:id])
   end
-
-  def destroy
-    mark = Mark.find(params[:id])
-    mark.destroy
-    redirect_to room_path(mark.map.room), status: :see_other, notice: "キズ（#{mark.brief_description}）を削除しました。"
-  end
 end
