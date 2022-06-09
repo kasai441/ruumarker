f29c1318bf6d9f66cd366e6ed8f623df444a1e34
kasai441 <kasai441441@gmail.com>
2022-06-09 15:41:13 +0900

    印刷バーをfixedにした #202

--- app/javascript/views/reports_index.vue
+++ app/javascript/views/reports_index.vue
@@ -1,13 +1,16 @@
 <template>
   <section>
     <div class="flex flex-col items-center">
-      <div class="w-full border-b border-slate-900/10 p-4 flex justify-center print:hidden">
+      <div class="fixed w-full z-10
+      border-b border-slate-900/10 p-4 bg-white
+      flex justify-center  print:hidden">
         <div class="w-field flex items-center">
           <a @click="room" class="w-1/6 text-xl text-slate-400 btn btn btn-ghost">＜</a>
           <h2 class="w-2/3 text-xl text-center">印刷用レイアウト</h2>
           <div class="w-1/6"></div>
         </div>
       </div>
+      <div class="relative h-20"></div>
       <div class="w-field font-bold text-lg p-6">キズ点検表</div>
       <image-show :room-id="roomId" :id="mapId" field-model="map" :image-url="mapImageUrl" :trimming="mapTrimming"
                   locators-model="mark" :locators-json="marks"
