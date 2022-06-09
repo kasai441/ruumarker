fc51db857f48391f95c37e208cb6fabd8dd7515d
kasai441 <kasai441441@gmail.com>
2022-06-09 14:48:26 +0900

    レポートコンポーネントを追加した #202

---
+++ app/javascript/views/reports_index.vue
@@ -0,0 +1,82 @@
+<template>
+  <section>
+    <div class="flex flex-col items-center pt-1">
+      <h2 class="w-field font-h2">キズ点検表print</h2>
+    </div>
+    <image-show :room-id="roomId" :id="mapId" field-model="map" :image-url="mapImageUrl" :trimming="mapTrimming"
+                locators-model="mark" :locators-json="marks"
+                @emit-form-data="getFormData"></image-show>
+    <div v-if="marksPresent">
+      <locators-index :room-id="roomId" :locators="marks" locators-model="mark"></locators-index>
+    </div>
+    <div v-else class="w-field h-28 rounded-lg bg-slate-100 flex justify-center items-center">
+      キズを追加できます
+    </div>
+    <img src="/new_mark.png" @click='newMark' @pointerdown="unbindHalfvanish" @pointerup="halfvanish"
+       id="create-mark" class="fixed z-10 new-mark" width="50">
+    <div class="h-20"></div>
+  </section>
+</template>
+
+<script>
+import ImageShow from '../components/image_show.vue'
+import LocatorsIndex from '../components/locators_index.vue'
+import tags from '../modules/tags'
+
+export default {
+  name: 'RoomsShow',
+  inject: [
+    'roomId',
+    'marks',
+    'map',
+  ],
+  data() {
+    return {
+      mapId: null,
+      mapImageUrl: null,
+      mapTrimming: null,
+      formData: null,
+      marksPresent: this.areMarks()
+    }
+  },
+  components: {
+    LocatorsIndex,
+    ImageShow
+  },
+  methods: {
+    getFormData(formData) {
+      this.formData = formData
+    },
+    newMark() {
+      location.href = `/rooms/${this.roomId}/marks/new`
+    },
+    halfvanish(e) {
+      tags.parent('IMG', e.target).classList.add('animate-halfvanish')
+    },
+    unbindHalfvanish(e) {
+      tags.parent('IMG', e.target).classList.remove('animate-halfvanish')
+    },
+    areMarks() {
+      return JSON.parse(this.marks).length > 0
+    },
+    print() {
+      location.href = `/rooms/${this.roomId}/reports`
+    },
+  },
+  created() {
+    const map = JSON.parse(this.map)
+    this.mapId = map['id']
+    this.mapImageUrl = map['image_url']
+    this.mapTrimming = map['trimming']
+  },
+  mounted() {
+    const download = document.getElementById('download')
+    download.classList.remove('hidden')
+    download.addEventListener('click', this.print)
+  },
+  beforeDestroy() {
+    const download = document.getElementById('download')
+    download.removeEventListener('click', this.print)
+  }
+}
+</script>
