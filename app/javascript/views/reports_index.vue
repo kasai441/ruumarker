f360d13abc4ca97fd4f4b4f17fb64c5c08af5964
kasai441 <kasai441441@gmail.com>
2022-06-10 10:44:23 +0900

    PDF出力の説明書きを追加した #202

--- app/javascript/views/reports_index.vue
+++ app/javascript/views/reports_index.vue
@@ -2,15 +2,26 @@
   <section>
     <div class="flex flex-col items-center">
       <div class="fixed w-full z-10
-      border-b border-slate-900/10 p-4 bg-black
-      flex justify-center print:hidden">
+      p-4 bg-black
+      flex flex-col items-center print:hidden">
         <div class="w-field flex items-center">
           <a @click="room" class="w-1/6 text-xl text-slate-400 btn btn btn-ghost">＜</a>
           <h2 class="w-2/3 text-xl text-white text-center">PDF用レイアウト</h2>
-          <a @click="print" class="w-1/6 btn btn-lime">プリント</a>
+          <div @click="print" class="w-1/6 btn btn-lime flex flex-auto py-0">
+            print
+          </div>
         </div>
       </div>
       <div class="relative h-20 print:hidden"></div>
+      <div class="w-full p-2 border-b border-slate-900/20 print:hidden">
+        <p class="text-slate-500">
+          「PRINT」から印刷ダイアログを表示してPDF出力を選択できます
+        </p>
+        <p class="text-slate-500">
+          ※ ダイアログが出ない場合、手動でブラウザの印刷画面を開いてください
+          <span class="text-lime-600">各ブラウザのPDF出力方法</span>
+        </p>
+      </div>
       <div class="w-field font-bold text-lg p-6">キズ点検表</div>
       <image-show :room-id="roomId" :id="mapId" field-model="map" :image-url="mapImageUrl" :trimming="mapTrimming"
                   locators-model="mark" :locators-json="marks"
