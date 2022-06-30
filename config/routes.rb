60dde9ba0e7c31225bed68258cd6c44f092efa60
kasai441 <kasai441441@gmail.com>
2022-06-30 14:43:03 +0900

    使用していないmarks#destroyとapi/marks#createのルート、marks#indexおよびmarks#createのメソッドを削除した

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,13 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :rooms, only: %i[new show destroy] do
     resources :maps, only: :edit
-    resources :marks, only: %i[new edit destroy]
+    resources :marks, only: %i[new edit]
     resources :reports, only: :index
   end
   namespace :api do
     resources :rooms, only: '' do
       resources :maps, only: %i[create update]
-      resources :marks, only: %i[create update destroy]
+      resources :marks, only: %i[update destroy]
     end
   end
 end
