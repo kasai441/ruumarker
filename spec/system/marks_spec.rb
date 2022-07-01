d062c73518cbbc50a8694da3447c2b073a5dcb82
kasai441 <kasai441441@gmail.com>
2022-07-01 13:59:41 +0900

    サンプルキズ画像を追加

--- spec/system/marks_spec.rb
+++ spec/system/marks_spec.rb
@@ -24,7 +24,7 @@ describe 'キズ管理機能', type: :system do
       it '登録され、キズ編集画面に遷移し、サンプルのキズ画像が表示される' do
         expect(Mark.all.count).to eq ex_marks_count + 1
         expect(page).to have_selector 'h2', text: 'キズの位置'
-        expect(find_by_id('edit-image')[:src]).to include 'sample.png'
+        expect(find_by_id('edit-image')[:src]).to include 'sample_locator.png'
       end
     end
   end
