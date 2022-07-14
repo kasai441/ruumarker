e31e64ca3beb1e19859ad742d0079edc19da9af2
kasai441 <kasai441441@gmail.com>
2022-07-14 14:50:11 +0900

    Jestの廃止 close #269

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globals: {},
-  transform: {
-    '\\.js$': 'babel-jest',
-    '\\.vue$': '@vue/vue3-jest'
-  },
-  moduleFileExtensions: ['vue', 'js', 'json'],
-  testPathIgnorePatterns: ['<rootDir>/vendor/'],
-}
