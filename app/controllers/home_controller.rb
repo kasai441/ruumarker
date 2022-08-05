fd3dfaa03e3bff890b112f2103b90bc99bd5f5a8
kasai441 <kasai441441@gmail.com>
2022-08-05 15:58:43 +0900

    cookieにroomIdが保存されていてDBからは期限切れで削除されている場合、Home画面でエラーになって動けなくなってしまうので、find_byにもどした #297

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,9 @@ class HomeController < ApplicationController
   def index
     Room.destroy_ten_days_old
     room_id = cookies.signed[:room_id]
-    return unless room_id
+    room = Room.find_by(id: room_id)
+    return unless room
 
-    redirect_to Room.find(room_id)
+    redirect_to room
   end
 end
