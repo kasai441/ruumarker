3bf7baa912b93d3b352587d43e925e68c694bf8c
kasai441 <kasai441441@gmail.com>
2022-08-05 16:29:44 +0900

    homeTopはホーム画面でしか意味ないので、場合分けの中に移動 #297

--- app/javascript/modules.js
+++ app/javascript/modules.js
@@ -4,11 +4,10 @@ window.addEventListener('popstate', () => {
   tags.browserBack()
 })
 
-tags.homeTop()
-
 const homeIndex = document.getElementById('home-index')
 const helpIndex = document.getElementById('help-index')
 if (homeIndex || helpIndex) {
+  tags.homeTop()
   tags.displayHome()
 } else {
   tags.displayHelp()
