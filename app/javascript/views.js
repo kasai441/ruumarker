ded25f9afe551d1e175a7de650b7de42d60138ad
kasai441 <kasai441441@gmail.com>
2022-08-11 15:05:15 +0900

    views.jsのコンポーネントの順番をアルファベット順にした #297

--- app/javascript/views.js
+++ app/javascript/views.js
@@ -1,29 +1,23 @@
 import { createApp } from 'vue'
-import RoomsShow from './views/rooms_show.vue'
-import RoomsNew from './views/rooms_new.vue'
 import MapsEdit from './views/maps_edit.vue'
 import MarksEdit from './views/marks_edit.vue'
 import ReportsIndex from './views/reports_index.vue'
+import RoomsNew from './views/rooms_new.vue'
+import RoomsShow from './views/rooms_show.vue'
 
 document.addEventListener('turbo:load', () => {
   const selectors = [
-    '#rooms-show',
-    '#rooms-new',
     '#maps-edit',
     '#marks-edit',
-    '#reports-index'
+    '#reports-index',
+    '#rooms-new',
+    '#rooms-show'
   ]
   selectors.forEach((selector) => {
     let app
     let element = document.querySelector(selector)
     if (element) {
       switch (element.id) {
-      case 'rooms-show':
-        app = createApp(RoomsShow)
-        break
-      case 'rooms-new':
-        app = createApp(RoomsNew)
-        break
       case 'maps-edit':
         app = createApp(MapsEdit)
         break
@@ -33,12 +27,18 @@ document.addEventListener('turbo:load', () => {
       case 'reports-index':
         app = createApp(ReportsIndex)
         break
+      case 'rooms-new':
+        app = createApp(RoomsNew)
+        break
+      case 'rooms-show':
+        app = createApp(RoomsShow)
+        break
       }
-      app.provide('roomId', element.getAttribute('room_id'))
-      app.provide('roomCreatedAt', element.getAttribute('room_created_at'))
       app.provide('map', element.getAttribute('map'))
       app.provide('marks', element.getAttribute('marks'))
       app.provide('mark', element.getAttribute('mark'))
+      app.provide('roomCreatedAt', element.getAttribute('room_created_at'))
+      app.provide('roomId', element.getAttribute('room_id'))
       app.mount(selector)
     }
   })
